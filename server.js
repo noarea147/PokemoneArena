b1cadc3af3e1dae1518b3bc19e1f3c1e672c0f89
noarea147 <95benmbarek@gmail.com>
2022-08-06 00:47:26 +0100

    heroku bug fix

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require('./app');
+const app = require("./app");
 
 const port = process.env.PORT_CLIENT || 3000;
-const server = require('http').createServer(app);
+const server = require("http").createServer(app);
 
-server.listen(port, () => {
+server.listen(port || 3000, () => {
   console.log(`Client Server started on port ${port}`);
 });
 
