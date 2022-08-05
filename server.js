2f600c5383e60c07ae9f2aca4e5fbe54a6a2055c
noarea147 <95benmbarek@gmail.com>
2022-08-06 00:50:35 +0100

    heroku

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = require("./app");
 const port = process.env.PORT_CLIENT || 3000;
 const server = require("http").createServer(app);
 
-server.listen(port || 3000, () => {
-  console.log(`Client Server started on port ${port}`);
-});
+server.listen(port || 3000);
 
 module.exports = server;
