5b365ffe7eaec23e860370959022fa110a49ddf2
noarea147 <95benmbarek@gmail.com>
2022-08-06 00:52:00 +0100

    port fixing

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = require("./app");
 const port = process.env.PORT_CLIENT || 3000;
 const server = require("http").createServer(app);
 
-server.listen(port || 3000);
+server.listen(port || 3000, () => {
+  console.log("Server is running on port " + port);
+});
 
 module.exports = server;
